33319bcb8a73ee2a4698878e11356a009110b6ce
오진혁 <hyeok7777@gmail.com>
2021-10-12 22:06:09 +0900

    feat(Application) : 게임 실행

--- src/main/java/racinggame/Application.java
+++ src/main/java/racinggame/Application.java
@@ -1,7 +1,15 @@
 package racinggame;
 
+import nextstep.utils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO 자동차 경주 게임 구현
+        GameRunner gameRunner = new GameRunner();
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String consoleReadline = Console.readLine();
+        System.out.println("시도할 회수는 몇회인가요?");
+        String tryTimes = Console.readLine();
+        gameRunner.startGame(consoleReadline, tryTimes);
     }
 }
